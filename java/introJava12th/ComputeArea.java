2fb231f17343a0905c26623316dacf13b1203bf9
ocsiker <qthang89vn@gmail.com>
2023-09-27 02:45:41 +0700

    chore: change method to get value radius

--- java/introJava12th/ComputeArea.java
+++ java/introJava12th/ComputeArea.java
@@ -1,5 +1,7 @@
 // compute area page 57/1717
 
+import java.util.Scanner;
+
 /** ComputeArea */
 public class ComputeArea {
 
@@ -8,12 +10,21 @@ public class ComputeArea {
     double radius, area;
 
     // SET radius to 20
-    radius = 20;
+    // radius = 20;
+
+    // create instance input from scanner
+    Scanner input = new Scanner(System.in);
+
+    // read radius from keyboard
+    radius = input.nextDouble();
 
     // compute area
     area = radius * radius * 3.14159;
 
     // display area
     System.out.println("This area for the circle of radius:" + radius + " is " + area);
+
+    // close input object
+    input.close();
   }
 }
