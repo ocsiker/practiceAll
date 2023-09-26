fdbfb81c07b80faafc7923cd49ed84161f15b6cb
ocsiker <qthang89vn@gmail.com>
2023-09-27 03:15:24 +0700

    feat: show notifcation to input radius

--- java/introJava12th/ComputeArea.java
+++ java/introJava12th/ComputeArea.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /** ComputeArea */
 public class ComputeArea {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     // read radius
     double radius, area;
 
@@ -14,6 +14,7 @@ public class ComputeArea {
     // create instance input from scanner
     Scanner input = new Scanner(System.in);
 
+    System.out.println("Input radius value: ");
     // read radius from keyboard
     radius = input.nextDouble();
 
