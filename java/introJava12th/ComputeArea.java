50594977d5c2dff8a88c20f6941f95d60a1d86cb
ocsiker <qthang89vn@gmail.com>
2023-09-26 08:26:06 +0700

    chore: delete dot file

---
+++ java/introJava12th/ComputeArea.java
@@ -0,0 +1,19 @@
+// compute area page 57/1717
+
+/** ComputeArea */
+public class ComputeArea {
+
+  public static void main(String[] args) {
+    // read radius
+    double radius, area;
+
+    // SET radius to 20
+    radius = 20;
+
+    // compute area
+    area = radius * radius * 3.14159;
+
+    // display area
+    System.out.println("This area for the circle of radius:" + radius + " is " + area);
+  }
+}
