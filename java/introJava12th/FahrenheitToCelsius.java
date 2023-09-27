2a5cebd33648b2caa0e8a6521423823ed5162371
ocsiker <qthang89vn@gmail.com>
2023-09-27 15:05:57 +0700

    feat: add fahrenheit convert to celsius

---
+++ java/introJava12th/FahrenheitToCelsius.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    // prompt user for input
+    System.out.println("Input value Fahrenheit to conver: ");
+    double fahrenheit = input.nextDouble();
+
+    double celsius = 5.0 / 9 * (fahrenheit - 32);
+    System.out.println("Fahrenheit" + fahrenheit + " is " + celsius + "celsius");
+
+    input.close();
+  }
+}
