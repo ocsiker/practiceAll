28ad7dfd44a1a906cd286839199d31eb502a5ca2
ocsiker <qthang89vn@gmail.com>
2023-09-27 22:25:41 +0700

    feat: add SaleTax

---
+++ java/introJava12th/SaleTax.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SaleTax {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    // Prompt user for input
+    System.out.println("Enter purchase amount:");
+    double purchaseAmount = input.nextDouble();
+
+    double tax = purchaseAmount * 0.06;
+
+    System.out.println("Sales tax is $" + (int) (tax * 100) / 100.0);
+    input.close();
+  }
+}
