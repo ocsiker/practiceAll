027e2d2b3492c36f6e242822dcb2918e8fdd2f1b
ocsiker <qthang89vn@gmail.com>
2023-09-27 15:24:14 +0700

    feat: convert milliTime to hours minutes seconds

---
+++ java/introJava12th/ShowCurrentTime.java
@@ -0,0 +1,31 @@
+public class ShowCurrentTime {
+  public static void main(String[] args) {
+
+    // get millis time from UNIX epoch
+    long milliTime = System.currentTimeMillis();
+
+    // Get totalSeconds by currentTimeMillis divide 1000
+    int totalSeconds = milliTime / 1000;
+
+    // seconds current by totalSeconds % 60
+    int seconds = totalSeconds % 60;
+
+    // totalMinutes current by totalSeconds / 60
+    int totalMinutes = totalSeconds / 60;
+
+    // seconds current by totalSeconds % 60
+    int minutes = totalMinutes % 60;
+
+    // hours current by totalMinutes / 60
+    int hours = totalMinutes / 60;
+    System.out.println(
+        milliTime
+            + " currentTimeMillis "
+            + hours
+            + " hours "
+            + minutes
+            + " minutes "
+            + seconds
+            + " seconds ");
+  }
+}
