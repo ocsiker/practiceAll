88c86d2088bc496584a6446e52c0ebc8ebbdc2ac
ocsiker <qthang89vn@gmail.com>
2023-09-27 15:07:11 +0700

    chore: typing error

--- java/introJava12th/FahrenheitToCelsius.java
+++ java/introJava12th/FahrenheitToCelsius.java
@@ -4,7 +4,7 @@ public class FahrenheitToCelsius {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     // prompt user for input
-    System.out.println("Input value Fahrenheit to conver: ");
+    System.out.println("Input value Fahrenheit to convert: ");
     double fahrenheit = input.nextDouble();
 
     double celsius = 5.0 / 9 * (fahrenheit - 32);
