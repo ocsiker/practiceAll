4cbe5efa0afd8312a786c76971b12d74ecc9a186
ocsiker <qthang89vn@gmail.com>
2023-10-03 14:10:52 +0700

    feat:exercise 3.15 complete

--- java/introJava12th/LotteryRevise315.java
+++ java/introJava12th/LotteryRevise315.java
@@ -4,6 +4,7 @@ public class LotteryRevise315 {
     java.util.Scanner input = new java.util.Scanner(System.in);
     // Generate three-digit integer
     int lotteryNumber = (int) (Math.random() * 1000);
+    System.out.println("Lottery number: " + lotteryNumber);
 
     int lotteryNumber1 = lotteryNumber / 100;
     int lotteryNumber2 = (lotteryNumber / 10) % 10;
@@ -16,8 +17,28 @@ public class LotteryRevise315 {
     int guessNumber1 = guessNumber / 100;
     int guessNumber2 = (guessNumber / 10) % 10;
     int guessNumber3 = guessNumber % 10;
-    boolean award2 = true;
-    boolean award3 = true;
+    // if incident 3 guess number equal 3 lottery number
+    boolean award2 =
+        ((lotteryNumber1 == guessNumber1)
+                || (lotteryNumber1 == guessNumber2)
+                || (lotteryNumber1 == guessNumber3))
+            && ((lotteryNumber2 == guessNumber1)
+                || (lotteryNumber2 == guessNumber2)
+                || (lotteryNumber2 == guessNumber3))
+            && ((lotteryNumber3 == guessNumber1)
+                || (lotteryNumber3 == guessNumber2)
+                || (lotteryNumber3 == guessNumber3));
+    // If you match one digit
+    boolean award3 =
+        ((lotteryNumber1 == guessNumber1)
+                || (lotteryNumber1 == guessNumber2)
+                || (lotteryNumber1 == guessNumber3))
+            || ((lotteryNumber2 == guessNumber1)
+                || (lotteryNumber2 == guessNumber2)
+                || (lotteryNumber2 == guessNumber3))
+            || ((lotteryNumber3 == guessNumber1)
+                || (lotteryNumber3 == guessNumber2)
+                || (lotteryNumber3 == guessNumber3));
 
     System.out.print("You're win $");
     if (lotteryNumber == guessNumber) {
