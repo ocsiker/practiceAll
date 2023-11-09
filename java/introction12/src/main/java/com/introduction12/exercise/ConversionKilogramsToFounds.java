65e03b1b8c2b0be0833e938bacc3e3725315d1dc
ocsiker <qthang89vn@gmail.com>
2023-11-09 08:55:20 +0700

    chore: edit format

--- java/introction12/src/main/java/com/introduction12/exercise/ConversionKilogramsToFounds.java
+++ java/introction12/src/main/java/com/introduction12/exercise/ConversionKilogramsToFounds.java
@@ -6,8 +6,9 @@ public class ConversionKilogramsToFounds {
     final double KILOGRAM_TO_FOUND = 2.2;
     // Display a table with 100 values
     for (int i = 1; i < 200; i += 2) {
-      System.out.print(i + "    ");
-      System.out.printf("%.1f", i * KILOGRAM_TO_FOUND);
+      System.out.printf("%-4d", i);
+      System.out.print("    ");
+      System.out.printf("%10.1f", i * KILOGRAM_TO_FOUND);
       System.out.println();
     }
 
