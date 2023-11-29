30ff851132b7ea556fe886c6c5b0625170cbac8a
ocsiker <qthang89vn@gmail.com>
2023-11-29 09:36:25 +0700

    feat:exercise 5.33

---
+++ java/introction12/src/main/java/com/introduction12/exercise/PerfectNumber.java
@@ -0,0 +1,24 @@
+public class PerfectNumber {
+  public static void main(String[] args) {
+    // Perfect number is number equal to the sum of the all of its divisors
+    // eg 6 = 3 + 2 + 1
+    // eg 28 = 14 + 7 + 4 + 1;
+    //
+    System.out.println("List the perfect number less  than 10000 is: ");
+    for (int number = 2; number < 10000; number++) {
+      int sumOfDivisors = 0;
+      for (int divisor = 1; divisor < number / 2 + 1; divisor++) {
+        if (number % divisor == 0) {
+          sumOfDivisors += divisor;
+        }
+        if (sumOfDivisors > number) {
+          break;
+        }
+      }
+
+      if (sumOfDivisors == number) {
+        System.out.printf("%d  ", number);
+      }
+    }
+  }
+}
