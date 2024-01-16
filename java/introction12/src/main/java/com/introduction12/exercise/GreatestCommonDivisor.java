22941a58bfb4ae0cd122dc500fa5da36d32425cf
ocsiker <qthang89vn@gmail.com>
2024-01-16 13:52:05 +0700

    refactor: gcd method

--- java/introction12/src/main/java/com/introduction12/exercise/GreatestCommonDivisor.java
+++ java/introction12/src/main/java/com/introduction12/exercise/GreatestCommonDivisor.java
@@ -1,4 +1,10 @@
+
+package com.introduction12.exercise;
+
 public class GreatestCommonDivisor {
+  /**
+   * Main method
+   */
   public static void main(String[] args) {
     // Create a Scanner
     java.util.Scanner input = new java.util.Scanner(System.in);
@@ -7,20 +13,27 @@ public class GreatestCommonDivisor {
     System.out.print("Enter number 1 and number 2 to find greatest common divisor: ");
     int number1 = input.nextInt();
     int number2 = input.nextInt();
-    // initial greatest common divisor with = 1
+
+    // Display the result
+    System.out.println("The greatest common divisor is: " + gcd(number1, number2));
+
+    input.close();]
+  }
+
+  /**
+   * greatest common divisor
+   */
+  public static int gcd(int num1, int num2) {
     int gcd = 1;
     // posiable gcd 2
     int k = 2;
-    while (k <= number1 && k <= number2) {
-      if (number1 % k == 0 && number2 % k == 0) {
+    while (k <= num1 && k <= num2) {
+      if (num1 % k == 0 && num2 % k == 0) {
         // update gcd
         gcd = k;
       }
       k++;
     }
-
-    // Display the result
-    System.out.println("The greatest common divisor is: " + gcd);
-
+    return gcd;
   }
 }
